cd0a90f46bf4f390cbf1dbc3fcd032ad498d491b
mosidrum <mosiokanga@gmail.com>
2025-02-19 00:32:34 +0100

    java exception handling

--- week5/ExceptionHandling.java
+++ week5/ExceptionHandling.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 class InvalidAgeException extends Exception {
     public InvalidAgeException(String message) {
         super(message);
-    }ception ha
+    }
 }
 
 public class ExceptionHandling {
